3b775c3ffbe8803baa265d072791a86a08b97be9
senchuu <senchuuuu@gmail.com>
2021-03-07 16:48:56 +0100

    ...Without the tests

--- lib/Logger.rb
+++ lib/Logger.rb
@@ -3,10 +3,3 @@ require_relative "Logger/colors"
 require_relative "Logger/constants"
 require_relative "Logger/writers"
 require_relative "Logger/String"
-
-Logger::Writer.info("Bonjour")
-Logger::Writer.warn("Bonjour")
-Logger::Writer.debug("Bonjour")
-Logger::Writer.comment("Bonjour")
-Logger::Writer.error("Bonjour")
-Logger::Writer.event("Bonjour")
